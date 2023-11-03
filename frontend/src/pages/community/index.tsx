683e74136924d66e73a8224feb33a288260517ba
Md. Jehadul Karim <sabit.jehadul.karim@gmail.com>
2023-11-03 23:29:33 +0600

    Community List Page

--- frontend/src/pages/community/index.tsx
+++ frontend/src/pages/community/index.tsx
@@ -1,5 +1,64 @@
 'use client'
 
-export default function Content() {
-  return <h1>Hello!</h1>
+import Link from 'next/link'
+import { useState } from 'react'
+
+export default function Communities() {
+  const [searchText, setSearchText] = useState('')
+
+  const communities: { title: string; description: string; id: string }[] = [
+    {
+      id: '1',
+      title: 'ArtBlock',
+      description: 'ArtBlock is a community of artists and developers pushing the boundaries of generative art.',
+    },
+    {
+      id: '2',
+      title: 'Portrait',
+      description: 'Portrait is a community of artists and developers pushing the boundaries of generative art.',
+    },
+  ]
+  return (
+    <>
+      <div className="flex p-4">
+        <div className="flex-1">
+          <h1 className="text-4xl font-bold">Communities</h1>
+        </div>
+        <div className="flex-none">
+          <input
+            type="text"
+            className="input input-bordered"
+            placeholder="Search"
+            value={searchText}
+            onChange={e => {
+              setSearchText(e.target.value)
+            }}
+          />
+        </div>
+      </div>
+      <div className="grid grid-cols-2 gap-4">
+        {communities
+          .filter(
+            community =>
+              community.title.toLowerCase().includes(searchText.toLowerCase()) ||
+              community.description.toLowerCase().includes(searchText.toLowerCase())
+          )
+          .map(community => (
+            <div className="card compact bg-base-100 p-4 shadow-lg" key={community.id}>
+              <div className="card-body">
+                <h2 className="card-title">{community.title}</h2>
+                <p>{community.description}</p>
+              </div>
+              <div className="card-footer">
+                <div className="flex justify-end">
+                  <Link href={`/community/${community.id}`}>
+                    <button className="btn btn-ghost">View</button>
+                  </Link>
+                </div>
+              </div>
+            </div>
+          ))}
+      </div>
+    </>
+  )
 }
--- frontend/src/pages/layout.tsx
+++ frontend/src/pages/layout.tsx
@@ -67,7 +67,26 @@ function Sidebar() {
                   (router.pathname === '/' ? ' bg-gray-100 dark:bg-gray-700' : '')
                 }
               >
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  strokeWidth={1.5}
+                  stroke="currentColor"
+                  className="h-5 w-5"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M2.25 15.75l5.159-5.159a2.25 2.25 0 013.182 0l5.159 5.159m-1.5-1.5l1.409-1.409a2.25 2.25 0 013.182 0l2.909 2.909m-18 3.75h16.5a1.5 1.5 0 001.5-1.5V6a1.5 1.5 0 00-1.5-1.5H3.75A1.5 1.5 0 002.25 6v12a1.5 1.5 0 001.5 1.5zm10.5-11.25h.008v.008h-.008V8.25zm.375 0a.375.375 0 11-.75 0 .375.375 0 01.75 0z"
+                  />
+                </svg>
+
                 <span className="ml-3">Dashboard</span>
+
+                <span className="ml-3 inline-flex items-center justify-center rounded-full bg-gray-200 px-2 text-sm font-medium text-gray-900 dark:bg-gray-900 dark:text-gray-300">
+                  NFTs
+                </span>
               </a>
             </Link>
           </li>
@@ -79,6 +98,20 @@ function Sidebar() {
                   (router.pathname.startsWith('/abx') ? ' bg-gray-100 dark:bg-gray-700' : '')
                 }
               >
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  strokeWidth={1.5}
+                  stroke="currentColor"
+                  className="h-5 w-5"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M8.25 7.5l.415-.207a.75.75 0 011.085.67V10.5m0 0h6m-6 0h-1.5m1.5 0v5.438c0 .354.161.697.473.865a3.751 3.751 0 005.452-2.553c.083-.409-.263-.75-.68-.75h-.745M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+                  />
+                </svg>
                 <span className="ml-3">ABX Marketplace</span>
               </a>
             </Link>
@@ -91,6 +124,17 @@ function Sidebar() {
                   (router.pathname.startsWith('/add-community') ? ' bg-gray-100 dark:bg-gray-700' : '')
                 }
               >
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  strokeWidth={1.5}
+                  stroke="currentColor"
+                  className="h-5 w-5"
+                >
+                  <path strokeLinecap="round" strokeLinejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
+                </svg>
+
                 <span className="ml-3"> Add Community</span>
               </a>
             </Link>
@@ -103,6 +147,21 @@ function Sidebar() {
                   (router.pathname.startsWith('/community') ? ' bg-gray-100 dark:bg-gray-700' : '')
                 }
               >
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  strokeWidth={1.5}
+                  stroke="currentColor"
+                  className="h-5 w-5"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M18 18.72a9.094 9.094 0 003.741-.479 3 3 0 00-4.682-2.72m.94 3.198l.001.031c0 .225-.012.447-.037.666A11.944 11.944 0 0112 21c-2.17 0-4.207-.576-5.963-1.584A6.062 6.062 0 016 18.719m12 0a5.971 5.971 0 00-.941-3.197m0 0A5.995 5.995 0 0012 12.75a5.995 5.995 0 00-5.058 2.772m0 0a3 3 0 00-4.681 2.72 8.986 8.986 0 003.74.477m.94-3.197a5.971 5.971 0 00-.94 3.197M15 6.75a3 3 0 11-6 0 3 3 0 016 0zm6 3a2.25 2.25 0 11-4.5 0 2.25 2.25 0 014.5 0zm-13.5 0a2.25 2.25 0 11-4.5 0 2.25 2.25 0 014.5 0z"
+                  />
+                </svg>
+
                 <span className="ml-3">Communities</span>
               </a>
             </Link>
