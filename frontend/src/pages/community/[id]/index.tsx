4b3d9873a14c2e33551ec554cbb4d65b3cf9ce95
Md. Jehadul Karim <sabit.jehadul.karim@gmail.com>
2023-11-04 02:21:40 +0600

    Get Community

--- frontend/src/pages/community/[id]/index.tsx
+++ frontend/src/pages/community/[id]/index.tsx
@@ -2,20 +2,37 @@
 import Link from 'next/link'
 import { useRouter } from 'next/router'
 
+import contractDetails from '../../../info/contractDetails.json'
+import {
+  useAccount,
+  useBalance,
+  useContractRead,
+  useContractWrite,
+  useNetwork,
+  usePrepareContractWrite,
+  useSwitchNetwork,
+  useWaitForTransaction,
+} from 'wagmi'
+import { ArtBlock__factory } from '../../../../typechain'
+import { useEffect, useState } from 'react'
+
 export default function CommunitySales() {
   const router = useRouter()
 
-  const community = {
-    id: '1',
-    title: 'ArtBlock',
-    description: 'ArtBlock is a community of artists and developers pushing the boundaries of generative art.',
-  }
+  const id = BigInt(router.query.id as string) || BigInt(0)
+
+  const { data: community } = useContractRead({
+    address: contractDetails.abxTokenContractAddress as `0x${string}`,
+    abi: ArtBlock__factory.abi,
+    functionName: 'getCommunity',
+    args: [id],
+  })
 
   return (
     <>
       <div className="flex p-4">
         <div className="flex-1">
-          <h1 className="text-4xl font-bold">{community.title}</h1>
+          <h1 className="text-4xl font-bold">{community?.title || 'Loading...'}</h1>
         </div>
         <div className="flex-none">
           <kbd className="kbd kbd-lg">Buy NFTs</kbd>
